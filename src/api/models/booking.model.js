2941eb0776fc1fb2df475849aeb4830d085ccb50
carlos <carlos.pacheco@bootcamp-upgrade.com>
2023-07-09 23:16:57 +0200

    updated models

--- src/api/models/booking.model.js
+++ src/api/models/booking.model.js
@@ -6,14 +6,13 @@ const Schema = mongoose.Schema;
 const bookingSchema = new Schema(
     {
         bookingCode: {type: String, required: true},
-        nameCustomer: {type: String, required: true},
-        lastNameCustomer: {type: String, required: true},
+        name: {type: String, required: true},
+        surname: {type: String, required: true},
         dateEntry: {type: String, required: true},
         dateDeparture: {type: String, required: true},
         nights: {type: Number, required: false},
         timeCheckin: {type: String, required: false},
-        commentCustomer: {type: String, required: false},
-        image: {type: String, required: false},
+        petitionCustomer: {type: String, required: false},
         room: {type: Schema.Types.ObjectId, ref: "booking", required: true},
     },{
         timestamps: true
